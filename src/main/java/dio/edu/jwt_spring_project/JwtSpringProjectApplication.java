636bbe68fa3d32f331d6beedafe8d1b838b65796
LUIZ NUNES DE ALMEIDA JUNIOR <l.n.almeida.ti@gmail.com>
2024-10-02 11:02:02 -0300

    project init

---
+++ src/main/java/dio/edu/jwt_spring_project/JwtSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package dio.edu.jwt_spring_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSpringProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/edu/jwt_spring_project/JwtSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.edu.jwt_spring_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
